93bddff1356639737869064072beffa4408baff1
DaniWinston25 <DaniWinston25@users.noreply.github.com>
2019-12-20 20:18:29 -0500

    updates

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import "./App.css";
 
-import Numbers from './components/ButtonComponents/NumberButtons/Numbers';
+import Numbers from './components/ButtonComponents/NumberButtons/Numbers.js';
 // STEP 4 - import the button and display components
 // Don't forget to import any extra css/scss files you build into the correct component
 
 // Logo has already been provided for you. Do the same for the remaining components
 import Logo from "./components/DisplayComponents/Logo";
+
 
 function App() {
   // STEP 5 - After you get the components displaying using the provided data file, write your state hooks here.
@@ -18,7 +19,7 @@ function App() {
   return (
     <div className="container">
       <Logo />
-      
+      <Numbers />
       <div className="App">
         {/* STEP 4 - Render your components here and be sure to properly import/export all files */}
       </div>
--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -2,9 +2,9 @@ import React,{useState} from "react";
 
 import {numbers} from "../../../data"
 
+import NumberButton from './NumberButton'
 
 
-const [nums, setNums] = useState(numbers);
 
 
 //import any components needed
@@ -15,12 +15,18 @@ import { numbers } from '../../../data'
 //Import your array data to from the provided data file
 
 const Numbers = () => {
-  // STEP 2 - add the imported data to state
+   // STEP 2 - add the imported data to state
+
+   const [nums] = useState(numbers);
+ 
   return (
     <div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
+   {nums.map((number, index) => {
+     return <NumberButton key = {index} number = {number}/>
+   })}    
     </div>
   );
 };
